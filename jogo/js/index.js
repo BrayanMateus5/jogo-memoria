13f0c775ab986565e8e7a2df9af74a2f2c2d1a1e
BrayanMateus5 <mateusbrayan73@gmail.com>
2026-02-26 20:49:48 -0300

    Duas aulas

---
+++ jogo/js/index.js
@@ -0,0 +1,74 @@
+let palavras = ["DIV", "META", "HEADER", "JS", "FOOTER", "BODY"];
+const url = "https://darkblue-frog-779608.hostingersite.com";
+const btnReiniciar = document.getElementById("btnReiniciar");
+const cards = document.querySelectorAll(".card");
+
+let primeira = null;
+let segunda = null;
+let tentativas = 0;
+
+
+buscarPalavras();
+
+async function buscarPalavras() {
+	try {
+		const response = await fetch(`${url}/api/palavras.php?quantidade=6`);
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		palavras = await response.json();
+		console.log(palavras);
+		iniciar();
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function iniciar() {
+	let embaralhadas = embaralhar([...palavras, ...palavras]);
+	cards.forEach((card, x) => {
+		card.textContent = "?";
+		card.dataset.palavra = embaralhadas[x];
+		card.onclick = () => virar(card);
+	});
+}
+
+function virar(card) {
+	card.textContent = card.dataset.palavra;
+	card.classList.add("selecionado");
+	if (!primeira) {
+		primeira = card;
+		return;
+	}
+	segunda = card;
+	tentativas++;
+	verificar();
+}
+
+function verificar() {
+	if (primeira.textContent == segunda.textContent) {
+		primeira = null;
+		segunda = null;
+		console.log("acertou...");
+	} else {
+		setTimeout(() => {
+			primeira.textContent = "?";
+			segunda.textContent = "?";
+			primeira.classList.remove("selecionado");
+			segunda.classList.remove("selecionado");
+			primeira = null;
+			segunda = null;
+		}, 600);
+	}
+}
+
+
+function embaralhar(array) {
+	for (let x = array.length - 1; x > 0; x--) {
+		let y = Math.floor(Math.random() * (1 + x));
+		[array[x], array[y]] = [array[y], array[x]];
+	}
+	return array;
+}
+
+btnReiniciar.onclick = () => buscarPalavras();
